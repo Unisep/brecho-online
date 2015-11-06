8537569c501f02beb51793e46937117a303dc848
Bruno <brunoocasali@gmail.com>
2015-11-06 02:26:24 -0200

    added other changes

--- WebContent/WEB-INF/js/controller_list.js
+++ WebContent/WEB-INF/js/controller_list.js
@@ -1,3 +1,14 @@
 shopApp.controller("ControllerList", function($scope, $location) {
-    
+    $scope.items = [
+        {
+            "images" : ["images/terno.png", "images/terno2.jpg", "images/terno3.jpg"],
+            "genre" : "masculino",
+            "rate_total" : 4.5,
+            "price" : 213.22,
+            "category" : { "name" : "Terno, gravata e sapatos" },
+            "owner" : { "name" : "Owner Name", "email": "owner@email.com", gender: "M" },
+            "description": "Lorem ipsum dolor sit amet, eos homero animal vivendo id, te eam quod graecis referrentur, vis ex stet eruditi. " +
+                           "Ei unum sale impedit nam, te eius abhorreant sed. Utamur luptatum ea cum, assum homero expet"
+        }
+    ]
 });
